bfba9e4e5dc834634c656de619fc1a3dcb9cd3ad
MOULIMONISH S <moulimonish19@gmail.com>
2025-02-10 15:44:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
+// backend/server.js
+
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import authRouter from "./routes/auth.js";
-import usersRouter from "./routes/users.js"; // Import users route
 import { connectDB } from "./config/db.js";
 
 dotenv.config();
 
+// Create express app
 const app = express();
 
+// Connect to the database
 connectDB();
 
+// Middleware to parse JSON bodies
 app.use(express.json());
 app.use(cors());
 
 // Routes
 app.use("/api/auth", authRouter);
-app.use("/api/users", usersRouter); // Add users route
 
+// Starting the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
